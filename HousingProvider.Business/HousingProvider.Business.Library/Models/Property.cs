6ad1fd6bbea0800ad296a37c3b30326abd902f5e
omar reid <omar.reid@verizon.net>
2017-07-12 17:51:36 -0400

    Created Property class and it's associated IProperty Interface. [#148776017]

---
+++ HousingProvider.Business/HousingProvider.Business.Library/Models/Property.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Business.Library.Models
+{
+    public class Property : IProperty
+    {
+    }
+}
