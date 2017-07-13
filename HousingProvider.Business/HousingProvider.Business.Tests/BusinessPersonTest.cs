35f807dc3b88f678f86dc4a8e62945bcfb77e0d6
omar reid <omar.reid@verizon.net>
2017-07-13 14:16:22 -0400

    Created NUnit test and verified GetFullName and Equals method returned the expected results. All test passed. [delivers #148774597]

---
+++ HousingProvider.Business/HousingProvider.Business.Tests/BusinessPersonTest.cs
@@ -0,0 +1,42 @@
+﻿using HousingProvider.Business.Library.Models;
+using NUnit.Framework;
+
+namespace HousingProvider.Business.Tests
+{
+   [TestFixture]
+    public class BusinessPersonTest
+    {
+        [Test]
+        public void GetFullNameTest()
+        {
+            var person = new Person { FirstName = "Omar", LastName = "Reid" };
+            var actual = person.GetFullName();
+
+            Assert.IsTrue(actual == "Omar Reid");
+        }
+        
+        [Test]
+        public void EqualsPositiveTest()
+        {
+            var person1 = new Person { FirstName = "Carl", LastName = "Paul" };
+            var person2 = new Person { FirstName = "Carl", LastName = "Carr" };
+            var person3 = new Person { FirstName = "Carl", LastName = "Carr" };
+
+            //Assert.IsTrue(person1.Equals(person2));
+
+            //Assert.IsFalse(person1.Equals(person2));
+
+            Assert.IsTrue(person2.Equals(person3));
+        }
+
+        [Test]
+        public void EqualsNegativeTest()
+        {
+            var person1 = new Person { FirstName = "Carl", LastName = "Paul" };
+            var person2 = new Person { FirstName = "Carl", LastName = "Carr" };
+            var person3 = new Person { FirstName = "Carl", LastName = "Carr" };
+
+            Assert.IsFalse(person1.Equals(person2));
+        }
+    }
+}
--- HousingProvider.Business/HousingProvider.Business.Tests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace HousingProvider.Business.Tests
-{
-    public class Class1
-    {
-    }
-}
