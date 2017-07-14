6fb4c8875601cfab74aae80fc63fe6c62ee598c8
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-14 10:27:45 -0400

    Modified ICrud interface to take in a Generic object T.  Updated RequestBroker to incorporate the Request Model from Business project. [#148884161]

--- HousingProvider.Business/HousingProvider.Business.Service/RequestBroker.cs
+++ HousingProvider.Business/HousingProvider.Business.Service/RequestBroker.cs
@@ -3,27 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HousingProvider.Business.Service.Interface;
+using HousingProvider.Business.Library.Models;
 
 namespace HousingProvider.Business.Service
 {
-    public class RequestBroker : ICrud
+    public class RequestBroker : ICrud<Request>
     {
-        public bool Create(object obj)
+
+        public bool Create(Request obj)
         {
             throw new NotImplementedException ();
         }
 
-        public bool Delete(object obj)
+        public bool Delete(Request obj)
         {
             throw new NotImplementedException ();
         }
 
-        public List<object> Get()
+        public List<Request> Get()
         {
             throw new NotImplementedException ();
         }
 
-        public bool Update(object obj)
+        public bool Update(Request obj)
         {
             throw new NotImplementedException ();
         }
