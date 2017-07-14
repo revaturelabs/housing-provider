dab8ef42f0f60911ad12679993ea0275b13a35a4
Julian Rojas <rojasmolinajulian@gmail.com>
2017-07-14 09:21:44 -0400

    Created RequestDAO [#148876169]

---
+++ HousingProvider.Data/HousingProvider.Data.Library/DAOModels/RequestDAO.cs
@@ -0,0 +1,36 @@
+﻿using HousingProvider.Data.Library.Interfaces;
+using HousingProvider.Data.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Data.Library.DAOModels
+{
+    class RequestDAO : ICrud<Request, string>
+    {
+        public bool Add(Request ObjectToBeAdded)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Request ObjectToBeDeleted)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Request Get(string obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Request> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Request ObjectToBeUpdated)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
