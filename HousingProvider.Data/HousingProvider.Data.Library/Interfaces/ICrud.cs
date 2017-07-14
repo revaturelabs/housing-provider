b0809e039f3c68040b599c493e2e87000e8b37d4
julianrojasm <julianrojasm_1993@hotmail.com>
2017-07-13 20:15:55 -0400

    Created Interface ICrud [#148876277]

---
+++ HousingProvider.Data/HousingProvider.Data.Library/Interfaces/ICrud.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Data.Library.Interfaces
+{
+    public interface ICrud<T>
+    {
+        T Get(T obj);
+        List<T> GetAll();
+        bool Add(T ObjectToBeAdded);
+        bool Delete(T ObjectToBeDeleted);
+        bool Update(T ObjectToBeUpdated);
+    }
+}
