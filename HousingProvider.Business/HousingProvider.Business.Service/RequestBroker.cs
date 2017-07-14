dd517d242330a16d04e28bcd5428156c94c0fd29
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-14 10:09:08 -0400

    Inherit ICrud interface [#148884161]

--- HousingProvider.Business/HousingProvider.Business.Service/RequestBroker.cs
+++ HousingProvider.Business/HousingProvider.Business.Service/RequestBroker.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HousingProvider.Business.Service.Interface;
 
 namespace HousingProvider.Business.Service
 {
-    public class RequestBroker
+    public class RequestBroker : ICrud
     {
+        public bool Create(object obj)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public bool Delete(object obj)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public List<object> Get()
+        {
+            throw new NotImplementedException ();
+        }
+
+        public bool Update(object obj)
+        {
+            throw new NotImplementedException ();
+        }
     }
 }
