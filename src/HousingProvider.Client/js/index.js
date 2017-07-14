8ef92a32d014443fed41d629e9ec34ea50788cbf
Lucas Carver <lucas.carver@outlook.com>
2017-07-14 13:03:07 -0400

    Made button to display the api object json [Finishes #148774295]

--- src/HousingProvider.Client/js/index.js
+++ src/HousingProvider.Client/js/index.js
@@ -4,7 +4,9 @@ var ng = require("angular");
 require("angular-route");
 var ngMainProvider = ng.module('ngMainProvider', ['ngRoute']);
 var request;
+var showMe = false;
 ngMainProvider.controller('Ctrl', function ($scope, $http) {
+    $scope.showMe = false;
     $http({
         method: "GET",
         url: "http://pokeapi.co/api/v2/pokemon/1"
@@ -15,8 +17,14 @@ ngMainProvider.controller('Ctrl', function ($scope, $http) {
         $scope.request = response.statusText;
     });
     $scope.showRequest = function () {
+        console.log(showMe);
         console.log("displaying Request...");
-        request = JSON.parse(request);
+        // if(typeof(request) != undefined)
+        //     {
+        //         console.log("passed if statement");
+        //         request = JSON.parse(request);
+        //     }
         console.log(request);
+        $scope.showMe = true;
     };
 });
--- src/HousingProvider.Client/ts/index.ts
+++ src/HousingProvider.Client/ts/index.ts
@@ -4,8 +4,9 @@ import 'angular-route';
 var ngMainProvider = ng.module('ngMainProvider', ['ngRoute']);
 
 var request;
-
+var showMe = false;
 ngMainProvider.controller('Ctrl', function ($scope, $http) {
+    $scope.showMe = false;
     $http({
         method: "GET",
         url: "http://pokeapi.co/api/v2/pokemon/1"
@@ -13,15 +14,24 @@ ngMainProvider.controller('Ctrl', function ($scope, $http) {
         $scope.request = response.data;
         request = response.data;
         
+        
     }, function error(response) {
         $scope.request = response.statusText;
     });
 
     $scope.showRequest = function() {
-
+        console.log(showMe);
         console.log("displaying Request...");
-        request = JSON.parse(request);
+        // if(typeof(request) != undefined)
+        //     {
+        //         console.log("passed if statement");
+        //         request = JSON.parse(request);
+        //     }
+            
         console.log(request);
+        
+        $scope.showMe = true;
+        
     }
 
 
