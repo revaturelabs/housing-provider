a6d3adaad9ddc7fc53aced374fd0b271444d3fd4
julianrojasm <julianrojasm_1993@hotmail.com>
2017-07-13 20:54:52 -0400

    Added NUnit to project and Tested The Entity Framework [delivers #148876277]

---
+++ HousingProvider.Data/HousingProvider.Data.Tests/EntityFrameworkTests.cs
@@ -0,0 +1,30 @@
+﻿using HousingProvider.Data.Library;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace HousingProvider.Data.Tests
+{
+    [TestFixture]
+    public class EntityFrameworkTests
+    {
+
+        [Test]
+        public void TestContext()
+        {
+            var myContext = ContextFactory.GetContext();
+
+            var request = myContext.Request.First();
+            Console.WriteLine(request.Description);
+
+            var result = myContext.Request.ToList().Count;
+
+
+            int expected = 0;
+
+            Assert.IsTrue(result > expected);
+        }
+    }
+}
