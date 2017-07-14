8b8b0c62197a2ac6cfc593cefc63d260c06c5379
Julian Rojas <rojasmolinajulian@gmail.com>
2017-07-14 09:27:13 -0400

    Created ProviderDAO [#148876169]

---
+++ HousingProvider.Data/HousingProvider.Data.Library/DAOModels/ProviderDAO.cs
@@ -0,0 +1,36 @@
+﻿using HousingProvider.Data.Library.Interfaces;
+using HousingProvider.Data.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Data.Library.DAOModels
+{
+    public class ProviderDAO : ICrud<Provider, string>
+    {
+        public bool Add(Provider ObjectToBeAdded)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Provider ObjectToBeDeleted)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Provider Get(string obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Provider> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Provider ObjectToBeUpdated)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
