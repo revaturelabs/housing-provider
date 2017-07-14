10b0a7a7b2ba8530d01d8cebc9f7caa0a9653070
Lucas Carver <lucas.carver@outlook.com>
2017-07-14 12:58:29 -0400

    Changed outDir to where js and script.bundle files are outputed [#148774295]

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   entry: './src/HousingProvider.Client/js/index.js',
   output: {
-    filename: './public/script.bundle.js'
+    filename: './src/HousingProvider.Client/public/script.bundle.js'
   }
 }
