a9c196db18f6d301fe32aec4cfaa786f85f0df45
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-14 10:54:39 -0400

    Modified Get function to handle Request objects. [#148839149]

--- HousingProvider.Business/HousingProvider.Business.Service/Controllers/RequestController.cs
+++ HousingProvider.Business/HousingProvider.Business.Service/Controllers/RequestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using HousingProvider.Business.Library.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,17 +14,17 @@ namespace HousingProvider.Business.Service.Controllers
     {
         // GET: api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Request> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new List<Request> ();
         }
 
-        // GET api/values/5
+        /*// GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
-        }
+        }*/
 
         // POST api/values
         [HttpPost]
@@ -31,7 +32,7 @@ namespace HousingProvider.Business.Service.Controllers
         {
         }
 
-        // PUT api/values/5
+        /*// PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
@@ -41,6 +42,6 @@ namespace HousingProvider.Business.Service.Controllers
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-        }
+        }*/
     }
 }
