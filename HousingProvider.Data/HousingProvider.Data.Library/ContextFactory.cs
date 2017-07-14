d009d64710dd6a0e50911b281a9da26c43a860b1
julianrojasm <julianrojasm_1993@hotmail.com>
2017-07-13 20:23:13 -0400

    Added Context Factory Class [#148876277]

---
+++ HousingProvider.Data/HousingProvider.Data.Library/ContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Data.Library
+{
+    public static class ContextFactory
+    {
+        private static HousingProviderDBContext _Context = new HousingProviderDBContext();
+
+        public static HousingProviderDBContext GetContext()
+        {
+            return _Context;
+        }
+    }
+}
