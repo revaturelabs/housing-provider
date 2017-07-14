03e0b4f8213f8e8e0e640d42576764b5bd640821
Julian Rojas <rojasmolinajulian@gmail.com>
2017-07-14 09:29:00 -0400

    Created PropertyDAO [#148876169]

---
+++ HousingProvider.Data/HousingProvider.Data.Library/DAOModels/PropertyDAO.cs
@@ -0,0 +1,36 @@
+﻿using HousingProvider.Data.Library.Interfaces;
+using HousingProvider.Data.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Data.Library.DAOModels
+{
+    public class PropertyDAO : ICrud<Property, Address>
+    {
+        public bool Add(Property ObjectToBeAdded)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Property ObjectToBeDeleted)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Property Get(Address obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Property> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Property ObjectToBeUpdated)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
