181826f638cb413a9b165fc831a676d3550b8ebb
Julian Rojas <rojasmolinajulian@gmail.com>
2017-07-14 09:32:18 -0400

    Created DaoHandler [#148876169]

---
+++ HousingProvider.Data/HousingProvider.Data.Library/DaoHandler.cs
@@ -0,0 +1,16 @@
+﻿using HousingProvider.Data.Library.DAOModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Data.Library
+{
+    public class DaoHandler
+    {
+        public PropertyDAO PropertyDao { get; set; }
+        public PersonDAO PersonDao { get; set; }
+        public ProviderDAO ProviderDao { get; set; }
+        public AddressDAO AddressDao { get; set; }
+        public RequestDAO RequestDao { get; set; }
+    }
+}
