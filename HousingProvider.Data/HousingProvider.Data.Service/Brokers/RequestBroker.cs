d39dd29fbeb9a0c98ee925d173999e18287a9eb8
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-15 23:12:33 -0400

    Created RequestBroker class [#148886051]

---
+++ HousingProvider.Data/HousingProvider.Data.Service/Brokers/RequestBroker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingProvider.Data.Service.Brokers
+{
+    public class RequestBroker
+    {
+    }
+}
