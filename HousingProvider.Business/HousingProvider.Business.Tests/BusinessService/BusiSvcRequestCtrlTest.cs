17dc8f2c74e73a80016d404d980cd466279406bb
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-17 09:52:24 -0400

    created class to hold all tests for Request Controller tests [#148865967]

---
+++ HousingProvider.Business/HousingProvider.Business.Tests/BusinessService/BusiSvcRequestCtrlTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Business.Tests.BusinessService
+{
+    class BusiSvcRequestCtrlTest
+    {
+    }
+}
