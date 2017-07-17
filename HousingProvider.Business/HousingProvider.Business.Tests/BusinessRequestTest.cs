feed0d693f03cb53051ba950601b3cde354b5c20
omar reid <omar.reid@verizon.net>
2017-07-17 09:53:27 -0400

    Tested Request class using RequestEquals Positive and Negative Test. All test passed. Work on Request class completed [delivers #148775557]

---
+++ HousingProvider.Business/HousingProvider.Business.Tests/BusinessRequestTest.cs
@@ -0,0 +1,61 @@
+﻿using HousingProvider.Business.Library.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Business.Tests
+{
+    [TestFixture]
+    public class BusinessRequestTest
+    {
+        [Test]
+        public void RequestEqualsPositiveTest()
+        {
+            var request1 = new Request {
+                Description = "Bedroom door broken",
+                Initiator = "Felice",
+                Provider = "Housing LLC",
+                DateSubmitted = DateTime.Parse("7/16/2017 10:00"),
+                Title = "Broken door",
+                Status = StatusEnum.PENDING
+            };
+
+            var request2 = new Request
+            {
+                Description = "Bedroom door broken",
+                Initiator = "Felice",
+                Provider = "Housing LLC",
+                DateSubmitted = DateTime.Parse("7/16/2017 10:00"),
+                Title = "Broken door",
+                Status = StatusEnum.INWORK
+            };
+
+            Assert.IsTrue(request1.Equals(request2));
+        }
+
+        [Test]
+        public void RequestEqualsNegativeTest()
+        {
+            var request1 = new Request
+            {
+                Description = "Bedroom door broken",
+                Initiator = "Felice",
+                Provider = "Housing LLC",
+                DateSubmitted = DateTime.Parse("7/16/2017 10:00"),
+                Title = "Broken door",
+            };
+
+            var request2 = new Request
+            {
+                Description = "Bedroom door Will not close",
+                Initiator = "Felice",
+                Provider = "Housing LLC",
+                DateSubmitted = DateTime.Parse("7/16/2017 10:00"),
+                Title = "Broken door",
+            };
+
+            Assert.IsFalse(request1.Equals(request2));
+         }
+   }
+}
