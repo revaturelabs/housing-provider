644de1b29652c0833b40a59eb7b594432270df9e
jameson burchette <jameson.burchette@outlook.com>
2017-07-18 11:46:59 -0400

    updated request data access object to use abstract data access base class [delivers #149040753]

--- HousingProvider.Data/HousingProvider.Data.Library/DAO/RequestDAO.cs
+++ HousingProvider.Data/HousingProvider.Data.Library/DAO/RequestDAO.cs
@@ -4,103 +4,20 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using HousingProvider.Data.Library.Abstracts;
+using Microsoft.EntityFrameworkCore;
 
 namespace HousingProvider.Data.Library.DAO
 {
-    public class RequestDAO : ICrud<Request, string>
+    public class RequestDAO : AEntityDataAccess<Request>
     {
-        private HousingProviderDBContext _Context = ContextFactory.GetContext();
+        public RequestDAO(DbContext context) : base(context) {}
 
-        public bool Add(Request ObjectToBeAdded)
+        public override Request Get(Request model)
         {
-            if(ObjectToBeAdded != null)
-            {
-                _Context.Request.Add(ObjectToBeAdded);
-                _Context.SaveChanges();
-                return true;
-            }
-
-            return false;
-        }
-
-        public bool Delete(Request ObjectToBeDeleted)
-        {
-            Request RequestInDb;
-            RequestInDb = _Context.Request.FirstOrDefault(x => x.RequestId == ObjectToBeDeleted.RequestId);
-
-            if(RequestInDb != null)
-            {
-                _Context.Request.Remove(RequestInDb);
-                _Context.SaveChanges();
-                return true;
-            }
-            return false;
-        }
-
-        public bool DeleteById(int id)
-        {
-            Request RequestInDb;
-            RequestInDb = _Context.Request.FirstOrDefault(x => x.RequestId == id);
-
-            if (RequestInDb != null)
-            {
-                _Context.Request.Remove(RequestInDb);
-                _Context.SaveChanges();
-                return true;
-            }
-            return false;
-        }
-
-        public Request Get(string title)
-        {
-            if(!string.IsNullOrEmpty(title))
-            {
-                Request request = _Context.Request.FirstOrDefault(x => x.RequestTitle == title);
-                if(request != null)
-                {
-                    return request;
-                }
-            }
-            return null;
-        }
-
-        public List<Request> GetAll()
-        {
-            List<Request> listRequests = _Context.Request.ToList();
-
-            if (listRequests != null)
-            {
-                return listRequests;
-            }
-
-            return null;
-
-        }
-
-        public Request GetById(int id)
-        {
-            var Request = _Context.Request.FirstOrDefault(x => x.RequestId == id);
-
-            if (Request != null)
-            {
-                return Request;
-            }
-
-            return null;
-        }
-
-        public bool Update(Request ObjectToBeUpdated)
-        {
-            Request RequestInDb;
-            RequestInDb = _Context.Request.FirstOrDefault(x => x.RequestId == ObjectToBeUpdated.RequestId);
-
-            if (RequestInDb != null)
-            {
-                RequestInDb = ObjectToBeUpdated;
-                _Context.SaveChanges();
-                return true;
-            }
-            return false;
+            return _Context.Set<Request>().FirstOrDefault(r => r.RequestTitle == model.RequestTitle &&
+                                                            r.RequestorName == model.RequestorName &&
+                                                            r.RepresentativeName == model.RepresentativeName);
         }
     }
 }
