a3718e42a6cefe6f05e866694be5366faa2eaa84
Ryan Pacovsky <Ryan.Pacovsky@gmail.com>
2017-07-18 15:55:27 -0400

    Create the Tests for the RequestType model. [delivers #149106693]

---
+++ HousingProvider.Data/HousingProvider.Data.Tests/RequestTypeTest.cs
@@ -0,0 +1,30 @@
+﻿using HousingProvider.Data.Service.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Data.Tests
+{
+    [TestFixture]
+    public class RequestTypeTest
+    {
+        [Test]
+        public void RequestTypeDescriptionTest()
+        {
+            var c = new RequestType();
+            c.RequestTypeDescription = "Supply Request";
+
+            Assert.IsNotNull(c.RequestTypeDescription);
+        }
+
+        [Test]
+        public void RequestTypeActiveTest()
+        {
+            var c = new RequestType();
+            c.Active = true;
+
+            Assert.IsTrue(c.Active);
+        }
+    }
+}
