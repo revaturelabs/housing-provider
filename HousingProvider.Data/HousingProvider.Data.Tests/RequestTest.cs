cb2688117011191c07855e559a893aa908331fc4
Ryan Pacovsky <Ryan.Pacovsky@gmail.com>
2017-07-18 16:27:15 -0400

    Create the tests for the Request model. [delivers #149106569]

---
+++ HousingProvider.Data/HousingProvider.Data.Tests/RequestTest.cs
@@ -0,0 +1,93 @@
+﻿using HousingProvider.Data.Service.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Data.Tests
+{
+    [TestFixture]
+    public class RequestTest
+    {
+        [Test]
+        public void RequestTitleTest()
+        {
+            var c = new Request();
+            c.RequestTitle = "Help";
+
+            Assert.IsNotNull(c.RequestTitle);
+        }
+
+        [Test]
+        public void RequestDescriptionTest()
+        {
+            var c = new Request();
+            c.Description = "Help";
+
+            Assert.IsNotNull(c.Description);
+        }
+
+        [Test]
+        public void RequestDateTest()
+        {
+            var c = new Request();
+            c.RequestDate = DateTime.Now;
+
+            Assert.IsNotNull(c.RequestDate);
+        }
+
+        [Test]
+        public void RequestDateModifiedTest()
+        {
+            var c = new Request();
+            c.RequestModifiedDate = DateTime.Now;
+
+            Assert.IsNotNull(c.RequestModifiedDate);
+        }
+
+        [Test]
+        public void RequestorNameTest()
+        {
+            var c = new Request();
+            c.RequestorName = "Help";
+
+            Assert.IsNotNull(c.RequestorName);
+        }
+
+        [Test]
+        public void RequestRepNameTest()
+        {
+            var c = new Request();
+            c.RepresentitiveName = "Help";
+
+            Assert.IsNotNull(c.RepresentitiveName);
+        }
+
+        [Test]
+        public void RequestActionTest()
+        {
+            var c = new Request();
+            c.Action = "Help";
+
+            Assert.IsNotNull(c.Action);
+        }
+
+        [Test]
+        public void RequestUrgentTest()
+        {
+            var c = new Request();
+            c.Urgent = true;
+
+            Assert.IsTrue(c.Urgent);
+        }
+
+        [Test]
+        public void RequestActiveTest()
+        {
+            var c = new Request();
+            c.Active = true;
+
+            Assert.IsTrue(c.Active);
+        }
+    }
+}
