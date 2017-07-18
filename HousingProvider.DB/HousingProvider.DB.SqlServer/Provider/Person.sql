674bda8f07803033263a58fcd6b9a4c34a81565b
myapham <myapham0410@gmail.com>
2017-07-17 20:28:34 -0400

    Added Person table under provider schema [#149048947]

---
+++ HousingProvider.DB/HousingProvider.DB.SqlServer/Provider/Person.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Provider].[Person] (
+    PersonId INT IDENTITY(1,1),
+    ProviderId INT NOT NULL,
+    FirstName NVARCHAR(50) NOT NULL,
+    LastName NVARCHAR(50) NOT NULL,
+    ContactId INT NOT NULL,
+    RoleId INT NOT NULL,
+    Active BIT NOT NULL DEFAULT(1)
+);
