9ba13c5969476ec7a6932a10b1cfb17af08beec6
myapham <myapham0410@gmail.com>
2017-07-17 23:23:40 -0400

    Added status table [#149054515]

---
+++ HousingProvider.DB/HousingProvider.DB.SqlServer/Provider/Status.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [Provider].[Status] (
+    StatusId INT primary key clustered IDENTITY(1,1) not null,
+    [StatusDescription] NVARCHAR(50) NOT NULL,
+    Active BIT NOT NULL DEFAULT(1)
+
+);
