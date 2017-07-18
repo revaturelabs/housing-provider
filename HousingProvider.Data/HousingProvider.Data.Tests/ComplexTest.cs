9d803e1c2ecace6f44887ccc150e321d5e005c55
Ryan Pacovsky <Ryan.Pacovsky@gmail.com>
2017-07-18 16:00:26 -0400

    Create the tests for the Complex model. [delivers #149106193]

---
+++ HousingProvider.Data/HousingProvider.Data.Tests/ComplexTest.cs
@@ -0,0 +1,39 @@
+﻿using HousingProvider.Data.Service.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Data.Tests
+{
+    [TestFixture]
+    public class ComplexTest
+    {
+        [Test]
+        public void ComplexNameTest()
+        {
+            var c = new Complex();
+            c.ComplexName = "Westerly";
+
+            Assert.IsNotNull(c.ComplexName);
+        }
+
+        [Test]
+        public void ComplexActiveTest()
+        {
+            var c = new Complex();
+            c.Active = true;
+
+            Assert.IsTrue(c.Active);
+        }
+
+        [Test]
+        public void ComplexAddressTest()
+        {
+            var c = new Complex();
+            c.Address = new Address();
+
+            Assert.IsNotNull(c.Address);
+        }
+    }
+}
