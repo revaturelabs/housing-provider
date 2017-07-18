c84da191908e93747ad0f3358fb956d437dfe8c0
Ryan Pacovsky <Ryan.Pacovsky@gmail.com>
2017-07-18 15:35:46 -0400

    Create the Contact model. [#149106281]

---
+++ HousingProvider.Data/HousingProvider.Data.Service/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingProvider.Data.Service.Models
+{
+    public class Contact
+    {
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool Active { get; set; }
+    }
+}
