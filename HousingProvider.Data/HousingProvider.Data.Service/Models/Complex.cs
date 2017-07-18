f90416f415a8698032098de3dd63601890802979
Ryan Pacovsky <Ryan.Pacovsky@gmail.com>
2017-07-18 15:32:09 -0400

    Create the Complex Model [#149106193]

---
+++ HousingProvider.Data/HousingProvider.Data.Service/Models/Complex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingProvider.Data.Service.Models
+{
+    public class Complex
+    {
+        public string ComplexName { get; set; }
+        public bool Active { get; set; }
+        public Address Address { get; set; }
+        public Contact Contact { get; set; }
+    }
+}
