800d69bcfe5aac1b0b9b337d77f867fd81b7652d
Ryan Pacovsky <Ryan.Pacovsky@gmail.com>
2017-07-18 16:17:20 -0400

    Create the Tests for the Person model. [delivers #149106343]

---
+++ HousingProvider.Data/HousingProvider.Data.Tests/PersonTest.cs
@@ -0,0 +1,39 @@
+﻿using HousingProvider.Data.Service.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Data.Tests
+{
+    [TestFixture]
+    public class PersonTest
+    {
+        [Test]
+        public void PersonFirstNameTest()
+        {
+            var c = new Person();
+            c.FirstName = "John";
+
+            Assert.IsNotNull(c.FirstName);
+        }
+
+        [Test]
+        public void PersonLastNameTest()
+        {
+            var c = new Person();
+            c.LastName = "Smith";
+
+            Assert.IsNotNull(c.LastName);
+        }
+
+        [Test]
+        public void PersonActiveTest()
+        {
+            var c = new Person();
+            c.Active = true;
+
+            Assert.IsTrue(c.Active);
+        }
+    }
+}
