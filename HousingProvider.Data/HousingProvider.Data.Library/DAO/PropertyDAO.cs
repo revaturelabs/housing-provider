3a9c1e9468d7b59368aeb975770d8fa197a213b9
jameson burchette <jameson.burchette@outlook.com>
2017-07-18 11:24:17 -0400

    updated property data access object to use abstract data access base class [#149040753]

--- HousingProvider.Data/HousingProvider.Data.Library/DAO/PropertyDAO.cs
+++ HousingProvider.Data/HousingProvider.Data.Library/DAO/PropertyDAO.cs
@@ -1,100 +1,28 @@
-﻿using HousingProvider.Data.Library.Interfaces;
+﻿using HousingProvider.Data.Library.Abstracts;
+using HousingProvider.Data.Library.Interfaces;
 using HousingProvider.Data.Library.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace HousingProvider.Data.Library.DAO
 {
-    public class PropertyDAO : ICrud<Property, Address>
+    public class PropertyDAO : AEntityDataAccess<Property>
     {
-        private HousingProviderDBContext _Context = ContextFactory.GetContext();
+        public PropertyDAO(DbContext context) : base(context) {}
 
-        public bool Add(Property ObjectToBeAdded)
+        public override Property Get(Property model)
         {
-            if (ObjectToBeAdded != null)
-            {
-                _Context.Property.Add(ObjectToBeAdded);
-                _Context.SaveChanges();
-                return true;
-            }
-
-            return false;
-        }
-
-        public bool Delete(Property ObjectToBeDeleted)
-        {
-            Property PropertyInDb;
-
-            PropertyInDb = _Context.Property.FirstOrDefault(x => x.AddressId == ObjectToBeDeleted.AddressId);
-
-            if(PropertyInDb != null)
-            {
-                _Context.Property.Remove(PropertyInDb);
-                _Context.SaveChanges();
-                return true;
-            }
-
-            return false;
-        }
-
-        public bool DeleteById(int id)
-        {
-            Property PropertyInDb;
-
-            PropertyInDb = _Context.Property.FirstOrDefault(x => x.PropertyId == id);
-
-            if (PropertyInDb != null)
-            {
-                _Context.Property.Remove(PropertyInDb);
-                _Context.SaveChanges();
-                return true;
-            }
-
-            return false;
-        }
-
-        public Property Get(Address obj)
-        {
-            if(obj != null)
-            {
-                return _Context.Property.FirstOrDefault(x => x.AddressId == obj.AddressId);
-            }
-
-            return null;
-        }
-
-        public List<Property> GetAll()
-        {
+            var mAdr = model.Address;
+            return _Context.Set<Property>().FirstOrDefault(p => p.Address.Street1 == mAdr.Street1 &&
+                                                            p.Address.Street2 == mAdr.Street2 &&
+                                                            p.Address.ApartmentNum == mAdr.ApartmentNum &&
+                                                            p.Address.City == mAdr.City &&
+                                                            p.Address.State == mAdr.State &&
+                                                            p.Address.ZipCode == mAdr.ZipCode);
             throw new NotImplementedException();
-        }
-
-        //id = PropertyId
-        public Property GetById(int id)
-        {
-            var Property = _Context.Property.FirstOrDefault(x => x.PropertyId == id);
-            if(Property !=null)
-            {
-                return Property;
-            }
-            return null;
-        }
-
-        public bool Update(Property ObjectToBeUpdated)
-        {
-            Property PropertyInDb;
-
-            PropertyInDb = _Context.Property.FirstOrDefault(x => x.PropertyId == ObjectToBeUpdated.PropertyId);
-
-            if (PropertyInDb != null)
-            {
-                PropertyInDb = ObjectToBeUpdated;
-                _Context.SaveChanges();
-                return true;
-            }
-
-            return false;
         }
     }
 }
