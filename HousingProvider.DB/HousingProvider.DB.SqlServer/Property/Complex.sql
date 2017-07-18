2a5609f5c1e299f36f654d76011560e80be2fc44
myapham <myapham0410@gmail.com>
2017-07-18 10:55:51 -0400

    Fixed complex table under property schema [#149083479]

--- HousingProvider.DB/HousingProvider.DB.SqlServer/Property/Complex.sql
+++ HousingProvider.DB/HousingProvider.DB.SqlServer/Property/Complex.sql
@@ -1,9 +1,9 @@
-﻿CREATE TABLE [Provider].[Complex] (
-    ComplexId INT primary key clustered IDENTITY(1,1),
+﻿CREATE TABLE [Property].[Complex] (
+    ComplexId INT primary key clustered IDENTITY(1,1) not null,
     ComplexName NVARCHAR(50) NOT NULL,
     AddressId INT NOT NULL,
     ContactId INT NOT NULL,
     Active BIT NOT NULL DEFAULT(1),
-	CONSTRAINT FK_Provider_complex_AddressId FOREIGN KEY (AddressId) REFERENCES [Provider].[Address](AddressId),
-	CONSTRAINT FK_Provider_complex_contactid FOREIGN KEY (ContactId) REFERENCES [Provider].[Contact](ContactId)
+	CONSTRAINT FK_Property_complex_AddressId FOREIGN KEY (AddressId) REFERENCES [Property].[Address](AddressId),
+	CONSTRAINT FK_Property_complex_contactid FOREIGN KEY (ContactId) REFERENCES [Person].[Contact](ContactId)
 );
