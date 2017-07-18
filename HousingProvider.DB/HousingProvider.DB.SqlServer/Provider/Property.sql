15ff9c96853d31b07d31d4a760c5164dec8c0648
myapham <myapham0410@gmail.com>
2017-07-17 22:30:14 -0400

    Added Property table under provider schema [#149053073]

---
+++ HousingProvider.DB/HousingProvider.DB.SqlServer/Provider/Property.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Provider].[Property] (
+    PropertyId INT IDENTITY(1,1),
+    NumberofRooms INT NOT NULL,
+    NumberofBaths INT NOT NULL,
+    ProviderId INT NOT NULL,
+    AddressId INT NOT NULL,
+    ComplexId INT NOT NULL,
+    Active BIT NOT NULL DEFAULT(1)
+);
