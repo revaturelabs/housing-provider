6dbca7a2ef95cc20ed76d2437833c07b20b17bf3
jameson burchette <jameson.burchette@outlook.com>
2017-07-17 23:23:46 -0400

    create abstract implementation of IDataAccess interface [#149040753]

---
+++ HousingProvider.Data/HousingProvider.Data.Library/Abstracts/AEntityDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using HousingProvider.Data.Library.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Data.Library.Abstracts
+{
+    public abstract class AEntityDataAccess<T> : IDataAccess<T> where T: class
+    {
+        public T Create(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(T model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
