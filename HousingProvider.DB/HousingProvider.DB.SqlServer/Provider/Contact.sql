48b002b8879964a8a2ed71f79e4b4c1a6d5f2b3f
myapham <myapham0410@gmail.com>
2017-07-17 21:27:03 -0400

    Added PK constraint to contact table [delivers #149051381]

--- HousingProvider.DB/HousingProvider.DB.SqlServer/Provider/Contact.sql
+++ HousingProvider.DB/HousingProvider.DB.SqlServer/Provider/Contact.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [Provider].[Contact] (
-    ContactId INT IDENTITY(1,1),
+    ContactId INT primary key clustered IDENTITY(1,1),
     Email NVARCHAR(50) NOT NULL,
     Phone NVARCHAR(50) NOT NULL,
     Active BIT NOT NULL DEFAULT(1)
+
 );
