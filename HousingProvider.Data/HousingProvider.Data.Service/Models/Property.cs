329acf2a01206e04c0a6d4d49234f97ccb0e2280
Ryan Pacovsky <Ryan.Pacovsky@gmail.com>
2017-07-18 16:03:46 -0400

    Created the Property model. [#149106445]

---
+++ HousingProvider.Data/HousingProvider.Data.Service/Models/Property.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingProvider.Data.Service.Models
+{
+    public class Property
+    {
+        public int NumberOfRooms { get; set; }
+        public int NumberOfBaths { get; set; }
+        public bool Active { get; set; }
+    }
+}
