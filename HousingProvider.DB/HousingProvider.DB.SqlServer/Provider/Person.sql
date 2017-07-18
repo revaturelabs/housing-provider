0680ebe949970350e7d1c9100eddc156750f8948
myapham <myapham0410@gmail.com>
2017-07-17 20:29:26 -0400

    Add PK contraint to person table [delivers #149048947]

--- HousingProvider.DB/HousingProvider.DB.SqlServer/Provider/Person.sql
+++ HousingProvider.DB/HousingProvider.DB.SqlServer/Provider/Person.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [Provider].[Person] (
-    PersonId INT IDENTITY(1,1),
+    PersonId INT primary key clustered IDENTITY(1,1),
     ProviderId INT NOT NULL,
     FirstName NVARCHAR(50) NOT NULL,
     LastName NVARCHAR(50) NOT NULL,
