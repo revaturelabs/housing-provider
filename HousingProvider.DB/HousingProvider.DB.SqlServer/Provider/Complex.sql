f5fbca3405af76c9406296e6e7b7ae6f81e7202a
myapham <myapham0410@gmail.com>
2017-07-17 22:23:13 -0400

    Added Complex table under provider schema [#149052911]

---
+++ HousingProvider.DB/HousingProvider.DB.SqlServer/Provider/Complex.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [Provider].[Complex] (
+    ComplexId INT IDENTITY(1,1),
+    ComplexName NVARCHAR(50) NOT NULL,
+    AddressId INT NOT NULL,
+    ContactId INT NOT NULL,
+    Active BIT NOT NULL DEFAULT(1)
+);
