785367225d91a65bbd961964e395ed3ded92a817
Ryan Pacovsky <Ryan.Pacovsky@gmail.com>
2017-07-18 16:21:47 -0400

    Create the Request model. [#149106569]

---
+++ HousingProvider.Data/HousingProvider.Data.Service/Models/Request.cs
@@ -0,0 +1,24 @@
+﻿using HousingProvider.Data.Library.EFModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingProvider.Data.Service.Models
+{
+    public class Request
+    {
+        public string RequestTitle { get; set; }
+        public string Description { get; set; }
+        public DateTime RequestDate { get; set; }
+        public DateTime RequestModifiedDate { get; set; }
+        public string RequestorName { get; set; }
+        public string RepresentitiveName { get; set; }
+        public string Action { get; set; }
+        public bool Urgent { get; set; }
+        public bool Active { get; set; }
+        public Property Property { get; set; }
+        public RequestType RequesType { get; set; }
+        public Status Status { get; set; }
+    }
+}
