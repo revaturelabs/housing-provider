86d7ae2e443d553bfae9643d5899e470498617a3
myapham <myapham0410@gmail.com>
2017-07-17 23:14:12 -0400

    Added request table [#149054215]

---
+++ HousingProvider.DB/HousingProvider.DB.SqlServer/Provider/Request.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [Provider].Request (
+    RequestId INT primary key clustered IDENTITY(1,1) not null,
+    RequestTypeId INT NOT NULL,
+    RequestTitle NVARCHAR(100) NOT NULL,
+    [Description] NVARCHAR(1000) NOT NULL,
+    RequestDate DATETIME NOT NULL DEFAULT(GETDATE()),
+    RequestModifiedDate DATETIME,
+    RequestorName NVARCHAR(50) NOT NULL,
+    PropertyId INT not null,
+    RepresentativeName NVARCHAR(50) NOT NULL,
+    StatusId INT NOT NULL,
+    [Action] NVARCHAR(1000),
+	 Urgent BIT NOT NULL DEFAULT(0),
+    Active BIT NOT NULL DEFAULT(1)
+
+    
+
+
+);
