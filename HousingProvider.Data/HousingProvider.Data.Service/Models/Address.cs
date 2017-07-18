522877a126a0d378bb01d308a7641266f38f2a28
Ryan Pacovsky <Ryan.Pacovsky@gmail.com>
2017-07-18 15:06:06 -0400

    Create the Address model [#149106085]

---
+++ HousingProvider.Data/HousingProvider.Data.Service/Models/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingProvider.Data.Service.Models
+{
+    public class Address
+    {
+        public string Street1 { get; set; }
+        public string Street2 { get; set; }
+        public int ApartmentNum { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public bool Active { get; set; }
+    }
+}
