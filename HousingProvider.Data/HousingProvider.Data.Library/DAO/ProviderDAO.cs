56093c2c97f87caa27dea88641d7570ff71f6792
jameson burchette <jameson.burchette@outlook.com>
2017-07-18 11:41:28 -0400

    updated provider data access object to use abstract data access base class [#149040753]

--- HousingProvider.Data/HousingProvider.Data.Library/DAO/ProviderDAO.cs
+++ HousingProvider.Data/HousingProvider.Data.Library/DAO/ProviderDAO.cs
@@ -1,98 +1,21 @@
-﻿using HousingProvider.Data.Library.Interfaces;
+﻿using HousingProvider.Data.Library.Abstracts;
+using HousingProvider.Data.Library.Interfaces;
 using HousingProvider.Data.Library.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace HousingProvider.Data.Library.DAO
 {
-    public class ProviderDAO : ICrud<Provider, string>
+    public class ProviderDAO : AEntityDataAccess<Provider>
     {
-        private HousingProviderDBContext _Context = ContextFactory.GetContext();
+        public ProviderDAO(DbContext context) : base(context) {}
 
-        public bool Add(Provider ObjectToBeAdded)
+        public override Provider Get(Provider model)
         {
-            if (ObjectToBeAdded != null)
-            {
-                _Context.Provider.Add(ObjectToBeAdded);
-                _Context.SaveChanges();
-                return true;
-            }
-
-            return false;
-        }
-
-        public bool Delete(Provider ObjectToBeDeleted)
-        {
-            Provider ProviderInDb;
-
-            ProviderInDb = _Context.Provider.FirstOrDefault( x => x.ProviderName == ObjectToBeDeleted.ProviderName);
-
-            if(ProviderInDb != null)
-            {
-                _Context.Provider.Remove(ProviderInDb);
-                _Context.SaveChanges();
-                return true;
-            }
-            return false;
-        }
-
-        public bool DeleteById(int id)
-        {
-            Provider ProviderInDb;
-
-            ProviderInDb = _Context.Provider.FirstOrDefault(x => x.ProviderId == id);
-
-            if (ProviderInDb != null)
-            {
-                _Context.Provider.Remove(ProviderInDb);
-                _Context.SaveChanges();
-                return true;
-            }
-            return false;
-        }
-
-        public Provider Get(string obj)
-        {
-            Provider Provider= _Context.Provider.FirstOrDefault(x => x.ProviderName == obj);
-            if(Provider != null)
-            {
-                return Provider;
-            }
-            return null;
-        }
-
-        public List<Provider> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Provider GetById(int id)
-        {
-            var Provider = _Context.Provider.FirstOrDefault(x => x.ProviderId == id);
-
-            if (Provider != null)
-            {
-                return Provider;
-            }
-
-            return null;
-        }
-
-        public bool Update(Provider ObjectToBeUpdated)
-        {
-            Provider ProviderInDb;
-
-            ProviderInDb = _Context.Provider.FirstOrDefault(x => x.ProviderId == ObjectToBeUpdated.ProviderId);
-
-            if (ProviderInDb != null)
-            {
-                ProviderInDb = ObjectToBeUpdated;
-                _Context.SaveChanges();
-                return true;
-            }
-            return false;
+            return _Context.Set<Provider>().FirstOrDefault(p => p.ProviderName == model.ProviderName);
         }
     }
 }
