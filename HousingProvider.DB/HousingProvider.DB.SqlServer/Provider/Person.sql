c2c93f584c1bbe748f12f5ec9e97c42eafbf4af4
myapham <myapham0410@gmail.com>
2017-07-17 22:36:49 -0400

    Updated constraints to person table [#149048947]

--- HousingProvider.DB/HousingProvider.DB.SqlServer/Provider/Person.sql
+++ HousingProvider.DB/HousingProvider.DB.SqlServer/Provider/Person.sql
@@ -5,5 +5,8 @@
     LastName NVARCHAR(50) NOT NULL,
     ContactId INT NOT NULL,
     RoleId INT NOT NULL,
-    Active BIT NOT NULL DEFAULT(1)
+    Active BIT NOT NULL DEFAULT(1),
+	 CONSTRAINT FK_Provider_Person_RoleId FOREIGN KEY (RoleId) REFERENCES [Provider].[Role](RoleId),
+	 CONSTRAINT FK_Provider_Person_ProviderId FOREIGN KEY (ProviderId) REFERENCES [Provider].[Provider](ProviderId),
+     CONSTRAINT FK_Provider_Person_ContactId FOREIGN KEY (ContactId) REFERENCES [Provider].[Contact](ContactId)
 );
