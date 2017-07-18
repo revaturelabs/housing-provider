ea8bef9c60545de10ba81c0d1a1af79ab402d8b7
jameson burchette <jameson.burchette@outlook.com>
2017-07-18 10:47:50 -0400

    updated person data access object to use abstract data access base class [#149040753]

--- HousingProvider.Data/HousingProvider.Data.Library/DAO/PersonDAO.cs
+++ HousingProvider.Data/HousingProvider.Data.Library/DAO/PersonDAO.cs
@@ -1,101 +1,22 @@
-﻿using HousingProvider.Data.Library.Interfaces;
+﻿using HousingProvider.Data.Library.Abstracts;
+using HousingProvider.Data.Library.Interfaces;
 using HousingProvider.Data.Library.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace HousingProvider.Data.Library.DAO
 {
-    public class PersonDAO : ICrud<Person, string>
+    public class PersonDAO : AEntityDataAccess<Person>
     {
-        private HousingProviderDBContext _Context = ContextFactory.GetContext();
+        public PersonDAO(DbContext context) : base(context) {}
 
-        public bool Add(Person ObjectToBeAdded)
+        public override Person Get(Person model)
         {
-            if (ObjectToBeAdded != null)
-            {
-                _Context.Person.Add(ObjectToBeAdded);
-                _Context.SaveChanges();
-                return true;
-            }
-
-            return false;
-        }
-
-        public bool Delete(Person ObjectToBeDeleted)
-        {
-            Person PersonInDb;
-
-            PersonInDb = _Context.Person.FirstOrDefault(x => (x.FirstName == ObjectToBeDeleted.FirstName) && (x.LastName == ObjectToBeDeleted.LastName));
-
-            if(PersonInDb != null)
-            {
-                _Context.Person.Remove(PersonInDb);
-                return true;
-            }
-            return false;
-        }
-
-        public bool DeleteById(int id)
-        {
-
-            Person PersonInDb;
-
-            PersonInDb = _Context.Person.FirstOrDefault(x => x.PersonId == id);
-
-            if (PersonInDb != null)
-            {
-                _Context.Person.Remove(PersonInDb);
-                return true;
-            }
-            return false;
-        }
-
-        public Person Get(string FullName)
-        {
-            char[] delimeter = { ' ' };
-            string[] names = FullName.Split(delimeter);
-
-            var Person = _Context.Person.FirstOrDefault(x => x.FirstName == names[0] && x.LastName == names[1]);
-
-            if(Person!=null)
-            {
-                return Person;
-            }
-            return null;
-        }
-
-        public List<Person> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Person GetById(int id)
-        {
-            var Person = _Context.Person.FirstOrDefault(x => x.PersonId == id);
-
-            if (Person != null)
-            {
-                return Person;
-            }
-
-            return null;
-        }
-
-        public bool Update(Person ObjectToBeUpdated)
-        {
-            Person PersonInDb;
-
-            PersonInDb = _Context.Person.FirstOrDefault(x => x.PersonId == ObjectToBeUpdated.PersonId);
-
-            if (PersonInDb != null)
-            {
-                PersonInDb = ObjectToBeUpdated;
-                _Context.SaveChanges();
-                return true;
-            }
-            return false;
+            return _Context.Set<Person>().FirstOrDefault(p => p.FirstName == model.FirstName && 
+                                                            p.LastName == model.LastName);
         }
     }
 }
