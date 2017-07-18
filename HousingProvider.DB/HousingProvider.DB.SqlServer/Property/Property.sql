633ef7f2241f2ae45736de6703ebd691e279ccaf
myapham <myapham0410@gmail.com>
2017-07-18 10:58:05 -0400

    Fixed property table under property schema [#149083479]

--- HousingProvider.DB/HousingProvider.DB.SqlServer/Property/Property.sql
+++ HousingProvider.DB/HousingProvider.DB.SqlServer/Property/Property.sql
@@ -1,12 +1,12 @@
-﻿CREATE TABLE [Provider].[Property] (
-    PropertyId INT primary key clustered IDENTITY(1,1),
+﻿CREATE TABLE [Property].[Property] (
+    PropertyId INT primary key clustered IDENTITY(1,1) not null,
     NumberofRooms INT NOT NULL,
     NumberofBaths INT NOT NULL,
     ProviderId INT NOT NULL,
     AddressId INT NOT NULL,
     ComplexId INT NOT NULL,
     Active BIT NOT NULL DEFAULT(1),
-	 CONSTRAINT FK_Provider_Property_ProviderId FOREIGN KEY (ProviderId) REFERENCES [Provider].[Provider](ProviderId),
-	 CONSTRAINT FK_Provider_Property_AddressId FOREIGN KEY (AddressId) REFERENCES [Provider].[Address](AddressId),
-	 CONSTRAINT FK_Provider_Property_ComplexId FOREIGN KEY (ComplexId) REFERENCES [Provider].[Complex](ComplexId)
+	 CONSTRAINT FK_Property_Property_ProviderId FOREIGN KEY (ProviderId) REFERENCES [Person].[Provider](ProviderId),
+	 CONSTRAINT FK_Property_Property_AddressId FOREIGN KEY (AddressId) REFERENCES [Property].[Address](AddressId),
+	 CONSTRAINT FK_Property_Property_ComplexId FOREIGN KEY (ComplexId) REFERENCES [Property].[Complex](ComplexId)
 );
