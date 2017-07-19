9098d6adc483abcaaad2a75fe1007a22f1b5ffb7
jameson burchette <jameson.burchette@outlook.com>
2017-07-18 20:39:10 -0400

    add factory class [#149104113]

---
+++ HousingProvider.Data/HousingProvider.Data.Library/Abstracts/ADataAccessFactory.cs
@@ -0,0 +1,31 @@
+﻿using HousingProvider.Data.Library.EFModels;
+using HousingProvider.Data.Library.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Data.Library.Abstracts
+{
+    public abstract class ADataAccessFactory
+    {
+        private static DbContext _Context;
+
+        public static DbContext Context
+        {
+            get
+            {
+                if (_Context == null)
+                {
+                    _Context = new HousingProviderDBContext();
+                }
+                return _Context;
+            }
+        }
+
+        public static IDataAccess<T> GetDAO<T>()
+        {
+            
+        }
+    }
+}
