e6235f0a1e14a2824db8ea51c94adedb60cb2baa
myapham <myapham0410@gmail.com>
2017-07-20 16:50:43 -0400

    Added test setup for Address table under property schema [#149262825]

---
+++ HousingProvider.DB/HousingProvider.DB.SqlServer/TestProcedures.sql
@@ -0,0 +1,15 @@
+﻿create procedure testSetup
+as
+begin
+	begin transaction
+		begin try
+			insert into Property.[Address] (Street1, Street2, ApartmentNum,City,[State], ZipCode)
+			values ('100 Test Street', '' ,'100', 'Test', 'VA', 22191);
+			commit tran;
+		end try
+		begin catch
+			rollback transaction;
+		end catch
+end;
+go
+
