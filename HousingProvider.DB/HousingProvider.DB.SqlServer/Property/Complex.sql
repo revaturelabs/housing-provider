782f58f9e0137722a9f4105876e53b948ddc3445
myapham <myapham0410@gmail.com>
2017-07-20 09:27:31 -0400

    Added Guid to Complex table under property schema [#149228393]

--- HousingProvider.DB/HousingProvider.DB.SqlServer/Property/Complex.sql
+++ HousingProvider.DB/HousingProvider.DB.SqlServer/Property/Complex.sql
@@ -4,6 +4,7 @@
     AddressId INT NOT NULL,
     ContactId INT NOT NULL,
     Active BIT NOT NULL DEFAULT(1),
-	CONSTRAINT FK_Property_complex_AddressId FOREIGN KEY (AddressId) REFERENCES [Property].[Address](AddressId),
+	[Guid ] UNIQUEIDENTIFIER NOT NULL, 
+    CONSTRAINT FK_Property_complex_AddressId FOREIGN KEY (AddressId) REFERENCES [Property].[Address](AddressId),
 	CONSTRAINT FK_Property_complex_contactid FOREIGN KEY (ContactId) REFERENCES [Person].[Contact](ContactId)
 );
