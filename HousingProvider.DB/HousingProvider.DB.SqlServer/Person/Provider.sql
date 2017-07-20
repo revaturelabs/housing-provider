685bcb885f2d70eb575fe09665305ff796841815
myapham <myapham0410@gmail.com>
2017-07-20 09:28:33 -0400

    Added Guid to Provider table under person schema [#149228393]

--- HousingProvider.DB/HousingProvider.DB.SqlServer/Person/Provider.sql
+++ HousingProvider.DB/HousingProvider.DB.SqlServer/Person/Provider.sql
@@ -2,6 +2,7 @@
     ProviderId INT primary key clustered IDENTITY(1,1) not null,
     ProviderName NVARCHAR(200) NOT NULL,
     AddressId INT NOT NULL,
-    Active BIT NOT NULL DEFAULT(1)
+    Active BIT NOT NULL DEFAULT(1), 
+    [Guid ] UNIQUEIDENTIFIER NOT NULL
 
 );
