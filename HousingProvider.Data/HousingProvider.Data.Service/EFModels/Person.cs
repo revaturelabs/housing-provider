6ec192b4dbfa1f17e8ef9d45ff90d30202cc4f85
jameson burchette <jameson.burchette@outlook.com>
2017-07-20 12:01:43 -0400

    regenerated entity framework models [#149231659]

---
+++ HousingProvider.Data/HousingProvider.Data.Service/EFModels/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HousingProvider.Data.Service.EFModels
+{
+    public partial class Person
+    {
+        public int PersonId { get; set; }
+        public int? ProviderId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ContactId { get; set; }
+        public bool Active { get; set; }
+        public Guid Guid { get; set; }
+
+        public virtual Contact Contact { get; set; }
+        public virtual Provider Provider { get; set; }
+    }
+}
