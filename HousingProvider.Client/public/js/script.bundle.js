487cb7232230f8e29a42f03dbc0edc7e0ddb056f
Yasha Taylor <yashataylor@gmail.com>
2017-07-20 13:00:02 -0400

    Added Jasmine test example. [delivers #149245043]

--- HousingProvider.Client/public/js/script.bundle.js
+++ HousingProvider.Client/public/js/script.bundle.js
@@ -69,6 +69,11 @@
 
 "use strict";
 
+Object.defineProperty(exports, "__esModule", { value: true });
+function addNumbers(n1, n2) {
+    return n1 + n2;
+}
+exports.addNumbers = addNumbers;
 
 
 /***/ })
