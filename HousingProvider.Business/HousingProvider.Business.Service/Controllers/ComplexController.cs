ee0f6d294caa50068ed6a75a514844ccdf7b4972
jameson burchette <jameson.burchette@outlook.com>
2017-07-20 21:29:59 -0400

    add apartment complex controller in the business service [#149277161]

---
+++ HousingProvider.Business/HousingProvider.Business.Service/Controllers/ComplexController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HousingProvider.Business.Library.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HousingProvider.Business.Service.Controllers
+{
+    [Route("api/[controller]")]
+    public class ComplexController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<Complex> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
