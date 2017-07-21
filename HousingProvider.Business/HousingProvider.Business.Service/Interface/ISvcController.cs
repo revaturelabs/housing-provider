44739f770244f04deb761e65f30db8fc36623e52
andre ballard <andre.ballard@outlook.com>
2017-07-21 12:19:55 -0400

    Refactored business logic controllers. Re-ran and passed tests [#149312451]

---
+++ HousingProvider.Business/HousingProvider.Business.Service/Interface/ISvcController.cs
@@ -0,0 +1,14 @@
+﻿using HousingProvider.Business.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingProvider.Business.Service.Interface
+{
+    public interface ISvcController<T>
+    {
+    bool Post();
+    List<T> Get();
+    }
+}
