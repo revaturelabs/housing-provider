9e13afd29d84219a01e09fd9fbdb7821e3b5f361
myapham <myapham0410@gmail.com>
2017-07-21 09:12:20 -0400

    Added Guid to Contact table [#149298907]

--- HousingProvider.DB/HousingProvider.DB.SqlServer/Person/Contact.sql
+++ HousingProvider.DB/HousingProvider.DB.SqlServer/Person/Contact.sql
@@ -2,6 +2,7 @@
     ContactId INT primary key clustered IDENTITY(1,1) not null,
     Email NVARCHAR(50) NOT NULL,
     Phone NVARCHAR(50) NOT NULL,
-    Active BIT NOT NULL DEFAULT(1)
+    Active BIT NOT NULL DEFAULT(1), 
+    [Guid] UNIQUEIDENTIFIER NOT NULL
 
 );
