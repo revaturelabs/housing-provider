6bf0abdadd875e17def6ac9317e1ac72edec6711
jameson burchette <jameson.burchette@outlook.com>
2017-07-20 22:49:41 -0400

    implemented the data service's complex controller's get method [finishes #149277161]

--- HousingProvider.Data/HousingProvider.Data.Service/Controllers/ComplexController.cs
+++ HousingProvider.Data/HousingProvider.Data.Service/Controllers/ComplexController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using HousingProvider.Data.Library.EFModels;
+using HousingProvider.Data.Library.DAO;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,7 +17,7 @@ namespace HousingProvider.Data.Service.Controllers
         [HttpGet]
         public IEnumerable<Complex> Get()
         {
-            return new Complex[] { new Complex { ComplexName = "Westerly"} };
+            return DataAccessFactory.GetComplexDAO().Read();
         }
     }
 }
