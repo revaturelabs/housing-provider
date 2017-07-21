cf173c68a09ac58d3ed8a257fa16764f12ef51bd
jameson burchette <jameson.burchette@outlook.com>
2017-07-20 21:21:41 -0400

    add apartment complex model [#149277161]

---
+++ HousingProvider.Business/HousingProvider.Business.Library/Models/Complex.cs
@@ -0,0 +1,15 @@
+﻿using HousingProvider.Business.Library.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Business.Library.Models
+{
+    public class Complex : ILibraryModel
+    {
+        public Guid Guid { get; set; }
+        public string ComplexName { get; set; }
+        public Address Address { get; set; }
+        public Contact Contact { get; set; }
+    }
+}
