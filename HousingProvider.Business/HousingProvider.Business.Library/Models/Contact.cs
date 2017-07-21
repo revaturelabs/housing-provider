3db76cb6eb631bdf1d3dbdfb74df55d82729d417
jameson burchette <jameson.burchette@outlook.com>
2017-07-20 21:27:16 -0400

    add contact model [#149277161]

---
+++ HousingProvider.Business/HousingProvider.Business.Library/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using HousingProvider.Business.Library.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Business.Library.Models
+{
+    public class Contact: ILibraryModel
+    {
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
