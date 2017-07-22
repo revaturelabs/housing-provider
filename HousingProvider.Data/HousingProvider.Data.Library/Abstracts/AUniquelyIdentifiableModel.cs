de63ae3e59e0c67f5f35a8ceea3523f6a8e50b6e
jameson burchette <jameson.burchette@outlook.com>
2017-07-22 18:27:47 -0400

    add abstract class AUniquelyIdentifiableModel with abstract guid property and ensured the entity framework models with guids extend it [#149351625]

---
+++ HousingProvider.Data/HousingProvider.Data.Library/Abstracts/AUniquelyIdentifiableModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Data.Library.Abstracts
+{
+    public abstract class AUniquelyIdentifiableModel
+    {
+        public abstract Guid Guid { get; set; }
+    }
+}
