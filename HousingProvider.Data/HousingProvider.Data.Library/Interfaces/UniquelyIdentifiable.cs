c49954f5c6d1ca5e4efaa61ea0cd9516ae3c3977
jameson burchette <jameson.burchette@outlook.com>
2017-07-22 17:10:29 -0400

    add uniquely identifiable interface that requires a guid property [#149351625]

---
+++ HousingProvider.Data/HousingProvider.Data.Library/Interfaces/UniquelyIdentifiable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingProvider.Data.Library.Interfaces
+{
+    public interface UniquelyIdentifiable
+    {
+        Guid guid { get; set; }
+    }
+}
