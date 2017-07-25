3e5e69f8d880eaca708942a32716510823e166c2
jameson burchette <jameson.burchette@outlook.com>
2017-07-25 14:58:49 -0400

    implemented get method of data service's address controller [#149512721]

--- HousingProvider.Data/HousingProvider.Data.Service/Controllers/AddressController.cs
+++ HousingProvider.Data/HousingProvider.Data.Service/Controllers/AddressController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using HousingProvider.Data.Service.Mappers;
+using HousingProvider.Data.Service.Models;
+using HousingProvider.Data.Library.DAO;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,13 +14,24 @@ namespace HousingProvider.Data.Service.Controllers
 {
     public class AddressController : ProviderDBController
     {
-        private EntityDTOMapper<Library.Models.Address, Models.Address> _Mapper = new EntityDTOMapper<Library.Models.Address, Models.Address>();
+        private EntityDTOMapper<Library.Models.Address, Address> _Mapper = new EntityDTOMapper<Library.Models.Address, Address>();
 
         public AddressController(DbContext context) : base(context) { }
 
-        public IActionResult Index()
+        [HttpGet("{guid}")]
+        public Address Get(string guid)
         {
-            return View();
+            var complex = DataAccessFactory.GetDataAccessObject<Library.Models.Complex>(Context).Find(Guid.Parse(guid), new string[] { "Address" });
+            var property = DataAccessFactory.GetDataAccessObject<Library.Models.Property>(Context).Find(Guid.Parse(guid), new string[] { "Address" });
+            if (complex != null)
+            {
+                return _Mapper.MapToDTO(complex.Address);
+            }
+            else if (property != null)
+            {
+                return _Mapper.MapToDTO(property.Address);
+            }
+            return null;
         }
     }
 }
