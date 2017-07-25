8b2424259deb108c753f76f780ecdefb164d9a7b
fred belotte <fred.belotte@revature.com>
2017-07-25 17:21:09 -0400

    refactor the client dependencies. [#149497565]

--- HousingProvider.Client/ngapp/index.ts
+++ HousingProvider.Client/ngapp/index.ts
@@ -1,5 +1,6 @@
 import * as ng from 'angular';
 import 'angular-route';
+import * as AuthenticationContext from 'adal-angular/lib/adal';
 import 'adal-angular/lib/adal-angular';
 import './css/index.css';
 import 'file-loader?name=[name].[ext]&outputPath=partials/!./partials/navbar-header.html';
