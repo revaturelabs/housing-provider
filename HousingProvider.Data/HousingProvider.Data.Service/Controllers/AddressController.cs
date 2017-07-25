15aad82189778665f760d4cda5f9ae83eb001deb
jameson burchette <jameson.burchette@outlook.com>
2017-07-25 14:42:53 -0400

    add address controller in the data service [#149512721]

---
+++ HousingProvider.Data/HousingProvider.Data.Service/Controllers/AddressController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HousingProvider.Data.Service.Mappers;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HousingProvider.Data.Service.Controllers
+{
+    public class AddressController : ProviderDBController
+    {
+        private EntityDTOMapper<Library.Models.Address, Models.Address> _Mapper = new EntityDTOMapper<Library.Models.Address, Models.Address>();
+
+        public AddressController(DbContext context) : base(context) { }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
