f534e57f049ea6f9150b069836e93cb29cc958e2
jameson burchette <jameson.burchette@outlook.com>
2017-07-25 15:00:16 -0400

    implemented ILibraryModel in business library's address model [#149512721]

--- HousingProvider.Business/HousingProvider.Business.Library/Models/Address.cs
+++ HousingProvider.Business/HousingProvider.Business.Library/Models/Address.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using HousingProvider.Business.Library.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HousingProvider.Business.Library.Models
 {
-    public class Address
+    public class Address : ILibraryModel
     {
         public string Address1 { get; set; }
         public string Address2 { get; set; }
