c1c592b30bc3ade88003ecadd1d6ea7956312969
jameson burchette <jameson.burchette@outlook.com>
2017-07-24 22:47:12 -0400

    implemented mapper config and maper method in complex mapper and used complex mapper in complex controller [finishes #149459365]

--- HousingProvider.Data/HousingProvider.Data.Service/Controllers/ComplexController.cs
+++ HousingProvider.Data/HousingProvider.Data.Service/Controllers/ComplexController.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using HousingProvider.Data.Library.Models;
+using HousingProvider.Data.Service.Models;
 using HousingProvider.Data.Library.DAO;
+using HousingProvider.Data.Service.Mappers;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,13 +15,22 @@ namespace HousingProvider.Data.Service.Controllers
     [Route("api/[controller]")]
     public class ComplexController : ProviderDBController
     {
+        private ComplexMapper _Mapper = new ComplexMapper();
+
         public ComplexController(HousingProviderDBContext context) : base(context) { }
 
         // GET: api/values
         [HttpGet]
-        public IEnumerable<Complex> Get()
+        public IEnumerable<Models.Complex> Get()
         {
-            return DataAccessFactory.GetDataAccessObject<Complex>(Context).Read();
+            var comList = DataAccessFactory.GetDataAccessObject<Library.Models.Complex>(Context).Read();
+            var dtos = new List<Models.Complex>();
+            foreach (var com in comList)
+            {
+                dtos.Add(_Mapper.MapToDTO(com));
+            }
+            return dtos;
+
         }
     }
 }
--- HousingProvider.Data/HousingProvider.Data.Service/Mappers/ComplexMapper.cs
+++ HousingProvider.Data/HousingProvider.Data.Service/Mappers/ComplexMapper.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using HousingProvider.Data.Library.Models;
+using HousingProvider.Data.Service.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,11 @@ namespace HousingProvider.Data.Service.Mappers
 {
     public class ComplexMapper
     {
+        private MapperConfiguration _ToDTO = new MapperConfiguration(cfg => cfg.CreateMap<Library.Models.Complex, Models.Complex>());
+
+        public Models.Complex MapToDTO(Library.Models.Complex com)
+        {
+            return _ToDTO.CreateMapper().Map<Models.Complex>(com);
+        }
     }
 }
