d3098a53e3af41a3bed4be405583a1f84f063916
myapham <myapham0410@gmail.com>
2017-07-27 12:52:53 -0400

    republished [#149578465]

--- HousingProvider.DB/HousingProvider.DB.SqlServer/Property/Complex.sql
+++ HousingProvider.DB/HousingProvider.DB.SqlServer/Property/Complex.sql
@@ -6,6 +6,8 @@
     Active BIT NOT NULL DEFAULT(1),
 	[Guid ] UNIQUEIDENTIFIER NOT NULL, 
     [ModifiedDate] DATETIME NOT NULL, 
+    [Url] NVARCHAR(MAX) NOT NULL, 
+    [Description] NVARCHAR(MAX) NOT NULL, 
     CONSTRAINT FK_Property_complex_AddressId FOREIGN KEY (AddressId) REFERENCES [Property].[Address](AddressId),
 	CONSTRAINT FK_Property_complex_contactid FOREIGN KEY (ContactId) REFERENCES [Person].[Contact](ContactId)
 );
