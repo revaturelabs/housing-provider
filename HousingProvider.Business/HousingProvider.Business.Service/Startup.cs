31ea8743f5e3520fca34ede74928b10768771670
jameson burchette <jameson.burchette@outlook.com>
2017-07-28 13:07:47 -0400

    made CORS more permissive [delivers #149740587]

--- HousingProvider.Business/HousingProvider.Business.Service/Startup.cs
+++ HousingProvider.Business/HousingProvider.Business.Service/Startup.cs
@@ -28,7 +28,7 @@ namespace HousingProvider.Business.Service
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddCors (o => o.AddPolicy ("default", b => b.AllowAnyOrigin ()));
+            services.AddCors(o => o.AddPolicy("default", b => b.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials().SetPreflightMaxAge(TimeSpan.FromSeconds(2520))));
             services.AddMvc();
         }
 
