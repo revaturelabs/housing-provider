7a203f636716bb12efc06c7e89a5917ca2aeb2b7
jameson burchette <jameson.burchette@outlook.com>
2017-08-02 21:51:56 -0400

    cleaned up client folder structure [#123456789]

--- HousingProvider.Client/ngapp/complex-create/module.ts
+++
@@ -1,5 +0,0 @@
-import * as ng from 'angular';
-
-var createComplex = ng.module('providerCreateComplex', []);
-
-export { createComplex };
--- HousingProvider.Client/ngapp/property-create/module.ts
+++
@@ -1,5 +0,0 @@
-import * as ng from 'angular';
-
-var createProperty = ng.module('providerCreateProperty', []);
-
-export { createProperty };
